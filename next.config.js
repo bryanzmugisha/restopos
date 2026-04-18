b71d2244cacac29cbdbab1fe611747f13cb16bc0
bryanzmugisha <bryanzmugisha@gmail.com>
2026-04-18 13:51:58 +0300

    Add deployment config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = { ...config.resolve.fallback, net: false, tls: false, fs: false }
+    }
+    return config
+  },
 }
-
 module.exports = nextConfig
